c06c7a5ba02ff418550811c807899ada60a92405
DeepSea3474 <redbluealking@hotmail.com>
2025-11-30 05:34:19 +0300

    Create İndex.js

---
+++ İndex.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// Eğer Web3 kodunuz için başka bir import gerekiyorsa buraya ekleyin (örneğin: 'ethers', 'web3modal')
+
+function Home() {
+  return (
+    // Bütün içeriğiniz TEK BİR ANA ETİKET (div, main, vb.) içinde olmalı
+    <div>
+      {/* Kendi Web3 bileşenlerinizin ve HTML kodunuzun TAMAMI BURAYA gelecek */}
+    </div>
+  );
+}
+
+export default Home;
