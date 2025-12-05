2950ba3addc56c25ca0b19386066068c7793ec4c
DeepSea3474 <Redbluealking@hotmail.com>
2025-12-05 09:23:01 +0100

    Fix index.js content and export

--- out/_next/static/hZelIR0SAtFJKum1OBUQ8/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-b646007f40c4f0a8.js"],"/index ":["static/chunks/pages/index -55cb1398ac455084.js"],sortedPages:["/_app","/_error","/index "]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>Aidag Chain Portal</h1>
+      <p>Welcome to the presale site!</p>
+      <p>Everything is now working correctly 🚀</p>
+    </div>
+  );
+}
