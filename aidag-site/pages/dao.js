4c45313fe847abce207f32ad7e5c502081e10a8f
DeepSea3474 <Redbluealking@hotmail.com>
2025-12-05 14:56:13 +0100

    feat: Yeni ve temiz DAO JSX bileşeni eklendi

---
+++ aidag-site/pages/dao.js
@@ -0,0 +1,8 @@
+export default function Dao() {
+  return (
+    <div>
+      <h1>DAO Sayfası</h1>
+      <p>Burası Aidag DAO (Merkeziyetsiz Otonom Organizasyon) ana içeriğidir.</p>
+    </div>
+  );
+}
