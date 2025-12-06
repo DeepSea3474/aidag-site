b096a754c527340443d8369aa53dab169a4441d6
DeepSea3474 <redbluealking@hotmail.com>
2025-12-06 16:57:11 +0100

    final: Disabled i18n config and postbuild script to force static Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  i18n: { // <-- output: 'export' satırı artık yok
-    locales: ['tr', 'en', 'de', 'fr', 'ar', 'ru', 'es', 'zh'],
-    defaultLocale: 'tr',
-    localeDetection: false,
-  },
+  // i18n bloğunu sildik.
+  reactStrictMode: true, 
 };
 
 module.exports = nextConfig;
+
