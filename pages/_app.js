44bfb36598c60eb19f0c7efbe8d2a047591644a7
DeepSea3474 <redbluealking@hotmail.com>
2026-01-04 18:53:54 +0100

    Fix _app.js imports and syntax

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/components.css'
 import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
@@ -11,4 +12,3 @@ export default function App({ Component, pageProps }) {
     </>
   )
 }
-
